44141174f73b4795cf0b2c101e3d13ae2681d595
bdnorris <bdnorris@gmail.com>
2015-08-04 07:15:38 -0500

    wrote some functions to shrink the code

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // ID of the Google Spreadsheet
- var spreadsheetID = "1wEJwYgjuz6Nd65As-6NOjaYrihDS9T2YVz2zrghBF6U";
+var spreadsheetID = "1wEJwYgjuz6Nd65As-6NOjaYrihDS9T2YVz2zrghBF6U";
  
- // Make sure it is public or set to Anyone with link can view 
+// Make sure it is public or set to Anyone with link can view 
 // the CALLBACK=X makes it JSONP which allows cross domain scripting
+// Security?
  var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json&amp;callback=x";
  
 $(document).ready(function() {
@@ -18,44 +19,34 @@ $(document).ready(function() {
         // We have an array of objects called entries
         // Now we're making a new array of just ones with 5 syllables
         var fives = $.grep(entries, function(value, i) {
-            return value.content.$t  == 'syllable: 5';
+            return value.gsx$syllable.$t  == 5;
         });
+
         // Another array of just ones with 7 syllables
         var sevens = $.grep(entries, function(value, i) {
-            return value.content.$t  == 'syllable: 7';
+            return value.gsx$syllable.$t  == 7;
         });
         
         // Get the lengths of each array
-        var fivesLength = fives.length;
-        var sevensLength = sevens.length;
-        console.log ("Fives: " + fivesLength);
-        console.log ("Sevens: " + sevensLength);
+        console.log ("Fives: " + fives.length);
+        console.log ("Sevens: " + sevens.length);
         
-        // Get a random number in those length
-        //var fivesRand1 = 44
-        var fivesRand1 = Math.floor(Math.random()*fivesLength)
-        console.log ("Five Random: " + fivesRand1);
-        
-        var sevensRand = Math.floor(Math.random()*sevensLength)
-        console.log ("Seven Random: " + sevensRand);
-        
-        //var fivesRand2 = 44
-        var fivesRand2 = Math.floor(Math.random()*fivesLength)
+        // Get a random number in those lengths
+        function randomNumber(min, max) {
+         return Math.floor(Math.random()*max);   
+        }
+        var fivesRand1 = randomNumber(0, fives.length);
+        var sevensRand = randomNumber(0, sevens.length);
+        var fivesRand2 = randomNumber(0, fives.length);
         
         // double check and make sure you're not getting the same random number for 1st and 3rd lines
         if (fivesRand1 == fivesRand2) {
-            var fivesRand2 = Math.floor(Math.random()*fivesLength)
-            //var fivesRand2 = 44
+            var fivesRand2 = randomNumber(0, fives.length);
             if (fivesRand1 == fivesRand2) {
-                var fivesRand2 = Math.floor(Math.random()*fivesLength)
+                var fivesRand2 = randomNumber(0, fives.length);
             }
         }
         
-        console.log ("Five Random: " + fivesRand2);
-        
-        
-        
-
         // My random div
         var holder1 = document.getElementById('random1');
         var holder2 = document.getElementById('random2');
@@ -64,11 +55,8 @@ $(document).ready(function() {
         holder1.textContent = fives[fivesRand1].gsx$phrase.$t;
         // Put a 7 in the holder
         holder2.textContent = sevens[sevensRand].gsx$phrase.$t;
-        // Put a 5 in the holder
+        // Put another 5 in the holder
         holder3.textContent = fives[fivesRand2].gsx$phrase.$t;
-        
-        
-        
         
         // PRINT STUFF OUT TO TEST
         /*
