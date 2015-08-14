68b53c31e3b6e65ec76f13dc577b32c6bc174ea2
bdnorris <bdnorris@gmail.com>
2015-08-13 21:15:17 -0500

    Trying to add a language filter. Not yet working.

--- app.js
+++ app.js
@@ -28,69 +28,149 @@ $.getJSON( url, { } )
             var sevens = $.grep(entries, function(value, i) {
                 return value.gsx$syllable.$t  == 7;
             });
+            var cleanFives = $.grep(entries, function(value, i) {
+                if (value.gsx$language.$t != 'x') {
+                    return value.gsx$syllable.$t == 5;
+                }
+            });
+            var cleanSevens = $.grep(entries, function(value, i) {
+                if (value.gsx$language.$t != 'x') {
+                    return value.gsx$syllable.$t == 7;
+                }
+            });
 
             // Get the lengths of each array
             console.log ("Fives: " + fives.length);
+            console.log ("Clean Fives: " + cleanFives.length);
             console.log ("Sevens: " + sevens.length);
+            console.log ("Clean Sevens: " + cleanSevens.length);
 
             // Get a random number in those lengths
             function randomNumber(min, max) {
              return Math.floor(Math.random()*max);   
             }
-            var fivesRand1 = randomNumber(0, fives.length);
-            var sevensRand = randomNumber(0, sevens.length);
-            var fivesRand2 = randomNumber(0, fives.length);
+            
 
+            n = $('#controls input:checked' ).length;
+            $("#controls #lanFilter").click(function() {
+                n = $('#controls input:checked' ).length;
+                console.log(n);
+            });
+            console.log(n);
+            
+ 
+            // if clean use clean arrays
+            var cleanfivesRand1 = randomNumber(0, cleanFives.length);
+            var cleansevensRand = randomNumber(0, cleanSevens.length);
+            var cleanfivesRand2 = randomNumber(0, cleanFives.length);
+                
             // double check and make sure you're not getting the same random number for 1st and 3rd lines
-            if (fivesRand1 == fivesRand2) {
-                var fivesRand2 = randomNumber(0, fives.length);
-                if (fivesRand1 == fivesRand2) {
-                    var fivesRand2 = randomNumber(0, fives.length);
+                if (cleanfivesRand1 == cleanfivesRand2) {
+                    var cleanfivesRand2 = randomNumber(0, cleanFives.length);
+                    if (cleanfivesRand1 == cleanfivesRand2) {
+                        var cleanfivesRand2 = randomNumber(0, cleanFives.length);
+                    }
                 }
-            }
 
-            // My random div
+                // if not clean use normal arrays
+                var fivesRand1 = randomNumber(0, fives.length);
+                var sevensRand = randomNumber(0, sevens.length);
+                var fivesRand2 = randomNumber(0, fives.length);
+                
+                // double check and make sure you're not getting the same random number for 1st and 3rd lines
+                if (fivesRand1 == fivesRand2) {
+                var fivesRand2 = randomNumber(0, fives.length);
+                    if (fivesRand1 == fivesRand2) {
+                        var fivesRand2 = randomNumber(0, fives.length);
+                    }
+                }
+            
+
+
+
+                            // My random div
             var holder1 = document.getElementById('random1');
             var holder2 = document.getElementById('random2');
             var holder3 = document.getElementById('random3');
+
+
+            if (n = 1) {
+                
+
+            // Put a 5 in the holder
+            holder1.textContent = cleanFives[cleanfivesRand1].gsx$phrase.$t;
+            // Put a 7 in the holder
+            holder2.textContent = cleanSevens[cleansevensRand].gsx$phrase.$t;
+            // Put another 5 in the holder
+            holder3.textContent = cleanFives[cleanfivesRand2].gsx$phrase.$t;
+                
+                            // CHANGE ON CLICK
+            // NEED TO ADD CHECKS TO MAKE SURE IT DOESN'T JUST REPLACE IT WITH THE SAME THING
+                function changeCleanFive() {
+                    var cleanfivesRand = randomNumber(0, cleanFives.length);
+                    return cleanFives[cleanfivesRand].gsx$phrase.$t;
+                }
+                function changeCleanSeven() {
+                    var cleansevensRand = randomNumber(0, cleanSevens.length);
+                    return cleanSevens[cleansevensRand].gsx$phrase.$t;
+                }
+            }
+            else {
+                
+
             // Put a 5 in the holder
             holder1.textContent = fives[fivesRand1].gsx$phrase.$t;
             // Put a 7 in the holder
             holder2.textContent = sevens[sevensRand].gsx$phrase.$t;
             // Put another 5 in the holder
             holder3.textContent = fives[fivesRand2].gsx$phrase.$t;
-
-
-            // CHANGE ON CLICK
+                
+                            // CHANGE ON CLICK
             // NEED TO ADD CHECKS TO MAKE SURE IT DOESN'T JUST REPLACE IT WITH THE SAME THING
-            function changeFive() {
-                var fivesRand = randomNumber(0, fives.length);
-                return fives[fivesRand].gsx$phrase.$t;
+                function changeFive() {
+                    var fivesRand = randomNumber(0, fives.length);
+                    return fives[fivesRand].gsx$phrase.$t;
+                }
+                function changeSeven() {
+                    var sevensRand = randomNumber(0, sevens.length);
+                    return sevens[sevensRand].gsx$phrase.$t;
+                }
             }
-            function changeSeven() {
-                var sevensRand = randomNumber(0, sevens.length);
-                return sevens[sevensRand].gsx$phrase.$t;
-            }
-            $(".five").click(function() {
-                this.textContent = changeFive();
-                return false;
-            });
-            $(".seven").click(function() {
-                this.textContent = changeSeven();
-                return false;
-            });
-/*            $('.five').on({
-    mouseover: function(){
-        $(this).css({filter: 'blur(2px)'});
-    },
-     mouseleave: function(){
-        $(this).css({filter: 'blur(0)'});
-    },
-    click: function(){
-        $(this).off({filter: 'blur(0)'});
-    }
-});
-*/
+            
+            
+                $(".five").click(function() {
+                    if (n = 1) {
+                        this.textContent = changeCleanFive();
+                    } else {
+                        this.textContent = changeFive();
+                    }
+                    return false;
+                });
+                $(".seven").click(function() {
+                    if (n = 1) {
+                        this.textContent = changeCleanSeven();
+                    } else {
+                        this.textContent = changeSeven();
+                    }
+                    return false;
+                });
+
+                $("#refresh").click(function() {
+                    if (n = 'tuna') {
+                        holder1.textContent = changeCleanFive();
+                        holder2.textContent = changeCleanFive();
+                        holder3.textContent = changeCleanSeven();
+                        console.log('clean function');
+                    } else {
+                        holder1.textContent = changeFive();
+                        holder2.textContent = changeFive();
+                        holder3.textContent = changeSeven();
+                        console.log('dirty function');
+                    }
+                    
+                    //return false;
+                });
+    
 
         });
 
@@ -99,3 +179,9 @@ $.getJSON( url, { } )
         var err = textStatus + ", " + error;
         console.log( "Request Failed: " + err );
     });
+
+
+// ANGULAR 
+
+var app = angular.module('haiku', []);
+
